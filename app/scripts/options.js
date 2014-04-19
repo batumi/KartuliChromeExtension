e917d7360da8a6dbd0cddccd3f961c6d75cb244e
cesine <cesine@yahoo.com>
2014-04-19 23:55:10 +0400

    on/off option saves

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+// Saves options to chrome.storage
+function saveOptions() {
+	var useTransliterator = document.getElementById('useTransliterator').checked;
+	chrome.storage.sync.set({
+		useTransliterator: useTransliterator
+	}, function() {
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function() {
+			status.textContent = '';
+		}, 1000);
+	});
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restoreOptions() {
+	document.getElementById('useTransliterator').addEventListener('change', saveOptions);
+	// Use default value color = 'red' and useTransliterator = true.
+	chrome.storage.sync.get({
+		useTransliterator: true
+	}, function(items) {
+		document.getElementById('useTransliterator').checked = items.useTransliterator;
+	});
+}
+document.addEventListener('DOMContentLoaded', restoreOptions);
