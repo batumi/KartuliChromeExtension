8de1b0a8239bd0a400c4f8ab9d02cc08916c1d46
cesine <cesine@yahoo.com>
2014-04-20 01:25:30 +0400

    transliterating every terminal text node
    if the user has transliteration turned on.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,12 +1,35 @@
 'use strict';
 
+
 chrome.storage.sync.get({
 	useTransliterator: true
 }, function(items) {
 	if (items.useTransliterator) {
-		console.log("TODO find text that was in latin letters and transliterate it");
-		console.log(Translitit);
+		window.setInterval(function(){
+			var walkTheDOM = function walk(node, func) {
+				func(node);
+				node = node.firstChild;
+				while (node) {
+					walk(node, func);
+					node = node.nextSibling;
+				}
+			};
+			walkTheDOM(document.body, function(node) {
+				if (node.nodeType === 1) {
+					// alert(node.id); // alert if we have a type 1 node
+					if (node.childElementCount === 0) {
+						var previousText = node.textContent ? node.textContent.trim() : "";
+						if (previousText) {
+							// console.log(previousText);
+							var changedText = Translitit(previousText);
+							node.innerHTML = changedText;
+							node.title = previousText;
+						}
+					}
+				}
+			});
+		}, 2000);
 	} else {
-		console.log("Transliteration is off");
+		console.log('Transliteration is off');
 	}
 });
